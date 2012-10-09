95b474b202c2842b01cf5cf581dfad171ccf72b6
marianona <mariaanona@gmail.com>
2012-10-09 11:32:15 +0300

    Update test_ufget_power_network.m

--- test_ufget_power_network.m
+++ test_ufget_power_network.m
@@ -130,7 +130,7 @@ while true
         tic;
         s = supermatrix(A);
         clear A;
-        s.fulliterate(adm, -1, minBlockSize, relativeError);
+        s = s.fulliterate(adm, -1, minBlockSize, relativeError);
         ttbuild = toc;
         tbuild(i) = ttbuild;
         
